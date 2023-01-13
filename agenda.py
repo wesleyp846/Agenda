69c9c329578285e3840de4663d79f41f20fb54cd
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-01-12 21:03:55 -0300

    Estrutura principal
    Criação da estrutura principal da agenda

--- agenda.py
+++ agenda.py
@@ -1,30 +1,83 @@
-AGENDA = {
-    'wesley': {
-        'tel': '97045-1118',
-        'email': 'aventureiro@gamil',
-        'endereco': 'avenida A',
-    },
-    'maria' : {
-        'tel': '965442-4268',
-        'email': 'wfpere86@gamil',
-        'endereco': 'avenida B',
-    },
-    'jonas': {
-        'tel': '99864-2158',
-        'email': 'csgrandro@gamil',
-        'endereco': 'avenida late',
-    }
+AGENDA = {}
+
+AGENDA['Wesley'] = {
+    'tel': '9541988-1118',
+    'email': 'aventureiro@gamil',
+    'endereco': 'avenida A',
 }
-# AGENDA['wesley']['endereco'] = 'rua manoel' # alterando valor da chave num dicionario
-# # print(AGENDA['wesley']['tel'])
-# # print(AGENDA['wesley']['endereco'])
-# #adicionando mais um contato
-# AGENDA['lucas'] = {
-#     'tel': '98592-4625',
-#     'email': 'smmfrco@gamil',
-#     'endereco': 'avenida paria',
-# }
-# print(AGENDA)
-# for contato in AGENDA:
-#     print(contato)
-# AGENDA.pop('lucas')
+
+AGENDA['Maria'] = {
+    'tel': '965442-4268',
+    'email': 'wfpere86@gamil',
+    'endereco': 'avenida B',
+}
+
+def mostrar_contatos():
+    print()
+    for contato in AGENDA:
+        buscar_contato(contato)
+
+def buscar_contato(contato):
+    print('Nome:', contato)
+    print('Telefone:', AGENDA[contato]['tel'])
+    print('E-mail:', AGENDA[contato]['email'])
+    print('Endereço:', AGENDA[contato]['endereco'])
+    print()
+
+def incluir_editar_contato(contato, tel, email, endereco):
+    AGENDA[contato] = {
+        'tel': tel,
+        'email': email,
+        'endereco': endereco,
+    }
+    print()
+    print(f'>>>>>>>>Contato {contato} adicionado com sucesso')
+    print()
+    print('__________________')
+
+def excluir_contato(contato):
+    AGENDA.pop(contato)
+    print()
+    print(f'>>>>>>>Contato {contato} excluido com sucesso')
+    print('__________________')
+    print()
+
+def imprimir_menu():
+    print('__________________')
+    print('1 - Mostrar todos os contatos')
+    print('2 - Buscar contato')
+    print('3 - Incluir contato')
+    print('4 - Editar contato')
+    print('5 - Excluir contato')
+    print('0 - Fechar agenda')
+    print('__________________')
+
+while True:
+    imprimir_menu()
+
+    opcao = input('Escolha uma opção: ')
+    print(opcao)
+
+    if opcao == '1':
+        mostrar_contatos()
+    elif opcao == '2':
+        contato = input('Digite o nome do contato: ')
+        buscar_contato(contato)
+    elif opcao == '3' or opcao == '4':
+        contato = input('Digite o nome do contato: ')
+        tel = input('Digite o telefone do contato: ')
+        email = input('Digite o e-mail do contato: ')
+        endereco = input('Digite o endereço do contato: ')
+        incluir_editar_contato(contato, tel, email, endereco)
+    elif opcao == '5':
+        contato = input('Digite o nome do contato: ')
+        excluir_contato(contato)
+    elif opcao == '0':
+        print()
+        print('>>>>Fechado o programa')
+        print()
+        break
+    else:
+        print()
+        print('>>>>>Opção inválida')
+        print()
