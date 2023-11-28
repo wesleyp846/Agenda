ab39f5656d1708f413862848784cce35c8e5ff6a
Wesley Pereira <aventureiro-msn@hotmail.com>
2023-11-28 18:03:16 -0300

    created buttons at interface graphic

--- graf.py
+++ graf.py
@@ -14,20 +14,31 @@ def main(page: ft.Page):
                 "/",
                 [
                     ft.AppBar(title=ft.Text("AGENDA"), bgcolor=ft.colors.SURFACE_VARIANT),
-                    ft.ElevatedButton("Incluir Contato", on_click=lambda _: page.go("/store")),
+                    ft.ElevatedButton("Mostar Contatos", on_click=lambda _: page.go("/mostar_contatos")),
+                    ft.ElevatedButton("Buscar Contatos", on_click=lambda _: page.go("/buscar_contatos")),
+                    ft.ElevatedButton("Incluir Contato", on_click=lambda _: page.go("/incluir_contato")),
+                    ft.ElevatedButton("Editar Contato", on_click=lambda _: page.go("/editar_contato")),
+                    ft.ElevatedButton("Excluir Contato", on_click=lambda _: page.go("/excluir_contato")),
+                    ft.ElevatedButton("Salvar Contato", on_click=lambda _: page.go("/salvar_contato")),
+                    ft.ElevatedButton("Importar Contatos", on_click=lambda _: page.go("/importar_contatos")),
+                    ft.ElevatedButton("Exportar Contatos", on_click=lambda _: page.go("/exportar_contatos")),
                 ],
             )
         )
-        if page.route == "/store":
+
+        if page.route == "/incluir_contato":
             page.views.append(
                 ft.View(
-                    "/store",
+                    "/incluir_contato",
                     [
                         ft.AppBar(title=ft.Text("Incluir Contato"), bgcolor=ft.colors.SURFACE_VARIANT),
                         ft.ElevatedButton("Pagina Inicial", on_click=lambda _: page.go("/")),
                     ],
                 )
             )
+
+        
+
         page.update()
 
     def view_pop(view):
